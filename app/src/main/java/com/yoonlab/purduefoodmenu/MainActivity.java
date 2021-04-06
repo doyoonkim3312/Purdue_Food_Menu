e8c72171734c01aececfed38f3319f0de7492db4
Ryan <556shooter4junk@gmail.com>
2021-04-06 17:16:14 -0400

    Commit [040621]
    * [FEAT] Add new click event on Button. So it will now change the text on textview as soon as button is clicked.

--- app/src/main/java/com/yoonlab/purduefoodmenu/MainActivity.java
+++ app/src/main/java/com/yoonlab/purduefoodmenu/MainActivity.java
@@ -7,12 +7,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //Activity is like page with in the book(App)
+
+    private boolean buttonClickStatus = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Protected = Kind of like a mid level between public and private
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //**********************************************************************************************
+        TextView Hello_World = findViewById(R.id.helloWorld_TextView);
+        Button Test_Button = findViewById(R.id.testBtn);
+
+
+        Test_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (buttonClickStatus == false) {
+                    Hello_World.setText("This is my first Android Application!");
+                    buttonClickStatus = true;
+                } else {
+                    Hello_World.setText("Hello World");
+                    buttonClickStatus= false;
+                }
+            }
+        });
     }
 }
