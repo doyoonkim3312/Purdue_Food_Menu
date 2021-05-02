24584ae2af9352b6f3506f9ab477c640a2bda567
Doyoon Kim <kim3312@purdue.edu>
2021-05-02 16:10:27 -0400

    Commit [050221] Refactor ViewPagerAdapter
    * [REFACTOR] Modify codes for instantiating fragments.

--- app/src/main/java/com/yoonlab/purduefoodmenu/Adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/yoonlab/purduefoodmenu/Adapter/ViewPagerAdapter.java
@@ -1,81 +1,48 @@
 package com.yoonlab.purduefoodmenu.Adapter;
 
 import android.content.Context;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.yoonlab.purduefoodmenu.Fragments.BreakfastFragment;
-import com.yoonlab.purduefoodmenu.Fragments.DinnerFragment;
-import com.yoonlab.purduefoodmenu.Fragments.LunchFragment;
-import com.yoonlab.purduefoodmenu.Models.Menu;
-import com.yoonlab.purduefoodmenu.apiService;
+import com.yoonlab.purduefoodmenu.Fragments.MenuDisplayFragment;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class ViewPagerAdapter extends FragmentStateAdapter {
 
     private Calendar calendar = Calendar.getInstance();
     private String courtName;
-    private Menu menuResult;
+    private String timePath;
+    // private Menu menuResult = new Menu();
 
     public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity, String courtName, Context context) {
         super(fragmentActivity);
         this.courtName = courtName;
 
         SimpleDateFormat mdyFormat = new SimpleDateFormat("MM-dd-yyyy");
-        String timePath = mdyFormat.format(calendar.getTime());
-
-        //Start API connection.
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.hfs.purdue.edu/")
-                .client(new OkHttpClient())
-                .addConverterFactory(SimpleXmlConverterFactory.create()).build();
-        Call<Menu> retrofitCall = retrofit.create(apiService.class).getMenu(courtName, timePath);
-        retrofitCall.enqueue(new Callback<Menu>() {
-            @Override
-            public void onResponse(Call<Menu> call, Response<Menu> response) {
-                if (response.isSuccessful()) {
-                    menuResult = response.body();
-                } else {
-                    Toast.makeText(context, "Failed: Menu cannot be loaded", Toast.LENGTH_LONG);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Menu> call, Throwable t) {
-                Toast.makeText(context, "CRITICAL", Toast.LENGTH_SHORT);
-            }
-        });
+        this.timePath = mdyFormat.format(calendar.getTime());
     }
 
     @NonNull
     @Override
     public Fragment createFragment(int position) {
 
-
         switch (position) {
             case 0: {
-                return new BreakfastFragment(menuResult);
+                return new MenuDisplayFragment(courtName, timePath, 0);
             }
             case 1: {
-                return new LunchFragment(menuResult);
+                return new MenuDisplayFragment(courtName, timePath, 1);
             }
             case 2: {
-                return new DinnerFragment(menuResult);
+                return new MenuDisplayFragment(courtName, timePath, 2);
             }
         }
-        return new BreakfastFragment(menuResult);
+        return new MenuDisplayFragment(courtName, timePath, 0);
     }
 
     @Override
