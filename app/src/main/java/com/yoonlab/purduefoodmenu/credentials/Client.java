4dab38123514d599b9abb2f657ca1cb986cba79b
Ryan <556shooter4junk@gmail.com>
2021-05-02 21:35:14 -0400

    CNIT 255 - Commit & Push test.
    Ryan - Test 3 - 9:34 PM

--- app/src/main/java/com/yoonlab/purduefoodmenu/credentials/Client.java
+++ app/src/main/java/com/yoonlab/purduefoodmenu/credentials/Client.java
@@ -1,5 +1,24 @@
 package com.yoonlab.purduefoodmenu.credentials;
 
-public class Client {
+public class Client extends User
+{
+    private UserType userType = UserType.CLIENT;
 
+    public Client(String password, String firstName, String lastName, String userID)
+    {
+        super(password, firstName, lastName, userID);
+        userType = UserType.CLIENT;
+    }
+
+
+    //Getter
+    public UserType getUserType() {
+        return userType;
+    }
+
+
+    //setter
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
--- app/src/main/java/com/yoonlab/purduefoodmenu/credentials/User.java
+++ app/src/main/java/com/yoonlab/purduefoodmenu/credentials/User.java
@@ -1,4 +1,54 @@
 package com.yoonlab.purduefoodmenu.credentials;
 
 public class User {
+
+    private String passwd;
+    private String fName;
+    private String lName;
+    private String UID;
+
+
+    public User(String password, String firstName, String lastName, String userID)
+    {
+        passwd = password;
+        fName = firstName;
+        lName = lastName;
+        UID = userID;
+    }
+
+
+    //GETTERS
+    public String getPassword() {
+        return passwd;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+
+    //SETTERS
+    public void setPassword(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
 }
